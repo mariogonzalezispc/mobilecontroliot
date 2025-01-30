4771327ce719ee4330d22730ebe66be741285236
Mario <mario.gonzalez@hotmail.com.ar>
2025-01-30 17:28:03 -0300

    Ajustes de botones

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-
-
 function menu() {
     const navMenu = document.querySelector(".nav_menu");
     const newRightValue = navMenu.style.right == "-10px" ? "-800px" : "-10px";
     setTimeout(() => {
       navMenu.style.right = newRightValue;
-      console.log('abrir menu')
       if (newRightValue === "-800px") {
         console.log('cerrar el menu')
+      }else{
+        console.log('abrir menu')
+      }
+    }, 250);
+  }
+
+  function productos_menu() {
+    const navMenu = document.querySelector(".produc");
+    const newRightValue = navMenu.style.left == "-10px" ? "-800px" : "-10px";
+    setTimeout(() => {
+      navMenu.style.right = newRightValue;
+      if (newRightValue === "-800px") {
+        console.log('cerrar el productos menu')
+      }else{
+        console.log('abrir productos menu')
+      }
+    }, 250);
+  }
+
+  function interfaces_menu() {
+    const navMenu = document.querySelector(".submenu_interfaces");
+    const newRightValue = navMenu.style.right == "-10px" ? "-800px" : "-10px";
+    setTimeout(() => {
+      navMenu.style.right = newRightValue;
+      if (newRightValue === "-800px") {
+        console.log('cerrar el interface menu')
+      }else{
+        console.log('abrir interfaces menu')       
       }
     }, 250);
   }
